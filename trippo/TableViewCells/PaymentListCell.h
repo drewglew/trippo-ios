26ba0dade8db71e67121bb7504e7dec9999be01e
Andrew Glew <drewglew@gmail.com>
2019-09-08 15:21:33 +0200

    2019-09-08 : Housekeeping

--- trippo/TableViewCells/PaymentListCell.h
+++ trippo/TableViewCells/PaymentListCell.h
@@ -6,8 +6,17 @@
 //  Copyright © 2018 andrew glew. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface PaymentListCell : NSObject
-
+@interface PaymentListCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *LabelDescription;
+@property (weak, nonatomic) IBOutlet UILabel *LabelHomeAmt;
+@property (weak, nonatomic) IBOutlet UILabel *LabelHomeCurrencyCode;
+@property (weak, nonatomic) IBOutlet UILabel *LabelLocalAmt;
+@property (weak, nonatomic) IBOutlet UILabel *LabelLocalCurrencyCode;
+@property (weak, nonatomic) IBOutlet UILabel *LabelHomeAmtEst;
+@property (weak, nonatomic) IBOutlet UILabel *LabelLocalAmtEst;
+@property (weak, nonatomic) IBOutlet UILabel *LabelHomeCurrencyCodeEst;
+@property (weak, nonatomic) IBOutlet UILabel *LabelLocalCurrencyCodeEst;
+@property (nonatomic) NSNumber *homeAmount;
 @end
